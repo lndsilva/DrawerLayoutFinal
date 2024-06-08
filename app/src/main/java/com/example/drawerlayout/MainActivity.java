ba8c517a7c23455a3727c883c9895bfb8e0ac066
Laercio <lndsilva@hotmail.com>
2024-06-07 21:31:22 -0300

    criando funcionalidades drawer

--- app/src/main/java/com/example/drawerlayout/MainActivity.java
+++ app/src/main/java/com/example/drawerlayout/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.drawerlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    Toolbar idToolBar;
+    DrawerLayout idDrawer;
+    NavigationView idNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
