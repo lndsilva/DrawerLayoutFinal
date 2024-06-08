8077c24c9a7ab640cd00ce7d6a5adb8eda4a7e55
Laercio <lndsilva@hotmail.com>
2024-06-07 21:50:45 -0300

    criando funcionalidades drawer novo

--- app/src/main/java/com/example/drawerlayout/MainActivity.java
+++ app/src/main/java/com/example/drawerlayout/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.drawerlayout;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar idToolBar;
+    MaterialToolbar idTopAppBar;
     DrawerLayout idDrawer;
     NavigationView idNavView;
 
@@ -17,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idTopAppBar = findViewById(R.id.idTopAppBar);
+
+        idDrawer = findViewById(R.id.idDrawer);
+
+        idTopAppBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this,idDrawer,idTopAppBar,R.string.open_drawer, R.string.close_drawer);
+                idDrawer.addDrawerListener(toggle);
+
+                toggle.syncState();
+            }
+        });
+
+
+
+
+
+
+
     }
 }
